dc77fffa696f02f0f448c8b76995d2d89bacf33e
Aniketh Sriramoju <99926055+Keen2640@users.noreply.github.com>
2026-06-21 18:54:41 -0500

    Revise comments and add includes in main.h
    Updated header file comments and included necessary headers.

--- main.h
+++ main.h
@@ -1,10 +1,16 @@
-/* *************************************************
-*  Name: Aniketh Sriramoju
-*  Assignment: Assignment 4
-*  Purpose: Header file for main driver (testbed for Stack)
-************************************************* */
+/*****************************************************************
+ * File: main.h
+ * Author: Aniketh Sriramoju
+ * Date: June 21, 2026
+ * Description: Includes required for the test driver in main.cpp.
+ *****************************************************************/
 
 #ifndef MAIN_H
 #define MAIN_H
 
-#endif // MAIN_H
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "stack.h"
+
+#endif
